0d10d924729f166746ccc62b05ebb1ab7fa8e056
Ezequiel Magalhaes <zekiel.dev@gmail.com>
2025-05-12 20:15:35 -0300

    feat: Salvando decisao

---
+++ decisao.py
@@ -0,0 +1,7 @@
+primeiro = float(input("Insira o primeiro número: "))
+segundo = float(input("Insira o segundo número: "))
+
+if(primeiro < segundo):
+    print ("Segundo é maior")
+else:
+    print ("Primeiro é maior")
