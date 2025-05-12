d832dafcb9f2d248f4569c3bc5bbb4c1ba69372b
LUCAS DE SOUSA ANDRADE <lucas.andrade00@cs.udf.edu.br>
2025-05-12 20:11:24 -0300

    feat: Adicionando a Sequencia

---
+++ sequencia.py
@@ -0,0 +1,11 @@
+# Programa que calcula a área de um retângulo
+
+# 1: Entrada dos Dados
+largura = float(input("Digite a largura do retângulo: "))
+altura = float(input("Digite a altura do retângulo: "))
+
+# 2: Processamento
+area = largura * altura
+
+# 3: Saída
+print("A área do retângulo é:", area)
